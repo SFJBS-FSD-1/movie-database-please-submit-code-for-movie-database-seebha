4478ec17d9dc621e5fdf48467def0a84205b1112
seebha <ntp3330@gmail.com>
2022-09-20 10:20:24 +0000

    Done

--- movie_app.py
+++ movie_app.py
@@ -91,7 +91,7 @@ class all_movies(Resource):
         print(data)
         # for i in data:
         #     print(i.title)
-        movie_list=[]
+        movie_list=["empty"]
         for i in data:
             movie_list.append({"title":i.title,"year":i.year,"genre":i.genre})
         return (movie_list)
